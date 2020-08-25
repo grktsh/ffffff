0b858e129e672b72b713785d782b8a4a93622130
Atsushi Hanaoka <grktsh@gmail.com>
2020-08-26 00:22:52 +0900

    Migrate from Travis to GitHub Actions (#5)

--- setup.py
+++ setup.py
@@ -1,8 +1,3 @@
 from setuptools import setup
 
-setup(
-    use_scm_version={
-        'write_to': 'ffffff/__init__.py',
-        'write_to_template': '__version__ = {version!r}\n',
-    }
-)
+setup()
